7bf26f9238d73fd1d5ed55532d9eaa736f65ab3b
Paulo-Pinto <48687163+Paulo-Pinto@users.noreply.github.com>
2021-07-11 13:18:27 +0100

    Readability

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import time
 import winsound
 import pandas as pd
 
-
+# todo : csv files that specify how long to wait until next beep
 def csv_to_sound(file_name="csv/beeps.csv"):
     data = pd.read_csv(file_name)
     for row in data.itertuples():
         # print(row)
         winsound.Beep(row[1], row[2])
 
-
+# old dial up sounds
 def dial_up(nr=11263):
     for i in [int(x) for x in str(nr)]:
         winsound.PlaySound("sounds/Dtmf" + str(i) + ".wav", winsound.SND_FILENAME)
 
-
+# test frequency/duration pairs, helpful for creating a good .csv file    
 def request_sound():
     freq, duration = 0, 0
     while int(freq := input("Frequency: ")) in range(0, 32000) \
@@ -22,7 +22,8 @@ def request_sound():
         winsound.Beep(int(freq), int(duration))
     print(f"Bad value, f {freq} d {duration}")
 
-
+# todo : create a pommodoro
+# beeps after a certain amount of time is elapsed (interval) throughout the duration
 def interval_reminder(duration=60, interval=10):
     """Beep reminder at an interval
 
@@ -34,16 +35,26 @@ def interval_reminder(duration=60, interval=10):
         print("Reminder played...")
         reminder_sound()
 
-
+# the custom alarm sound, gets annoying fast :p
 def reminder_sound():
     for j in range(1, 6):
         winsound.Beep(2000 + j * 300, 50)
 
 
 if __name__ == '__main__':
+    # play the windows alert sound     
     # winsound.PlaySound("*", winsound.SND_ALIAS)
 
+    # play the default .csv file     
     # csv_to_sound()
+    
+    # test your own phone number
     # dial_up(input("Type your phone number: "))
+    
+    # test how some frequency would sound on some duration
     # request_sound()
-    interval_reminder()
+    
+    # beep every 10 minutes for an hour
+    # interval_reminder()
+    
+    pass
